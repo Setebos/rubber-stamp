da7cb267628dcf9e109fb28afb299abba3524aed
Fabien Trebouvil <f.trebouvil-exterieur@ldlc.com>
2018-05-17 11:46:45 +0200

    ajout du choix de couleur du tampon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const text   = document.getElementById('stamp-text');
+const color  = document.getElementById('stamp-color');
 const result = document.getElementById('stamp-result');
+
+color.addEventListener('change', function(e) {
+	result.style.color       = color.value;
+	result.style.borderColor = color.value;
+})
 
 text.addEventListener('keyup', function(e) {
 	result.textContent = text.value;
