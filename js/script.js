2f6b6c010bdaea557ac955e0566dc401d07b030a
Fabien Trebouvil <f.trebouvil-exterieur@ldlc.com>
2018-05-17 14:35:12 +0200

    ajout de la possibilité d'exporter le tampon en png

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-const text   = document.getElementById('stamp-text');
-const color  = document.getElementById('stamp-color');
-const result = document.getElementById('stamp-result');
+const text    = document.getElementById('stamp-text');
+const color   = document.getElementById('stamp-color');
+const result  = document.getElementById('stamp-result');
+const button = document.getElementById('stamp-convert');
+const convert = document.getElementById('convert-div');
 
 color.addEventListener('change', function(e) {
 	result.style.color       = color.value;
@@ -10,3 +12,9 @@ color.addEventListener('change', function(e) {
 text.addEventListener('keyup', function(e) {
 	result.textContent = text.value;
 })
+
+button.addEventListener('click', function() {
+	html2canvas(convert).then(function(canvas) {
+		return Canvas2Image.saveAsPNG(canvas)
+	})
+});
